2c9de7bc4b52b02e152610c1ab761340ed825b23
Mazzie <mazziechai@gmail.com>
2022-07-17 12:19:43 -0700

    Implement fuzzy search command 🎉

--- hun_kawaba/bot.py
+++ hun_kawaba/bot.py
@@ -1,14 +1,15 @@
 import traceback
 
 import discord
-from discord.ext import bridge, commands
+from discord.ext import bridge, commands, pages
 from discord.utils import escape_markdown
+from thefuzz import process
 
 import config
 
 from .logger import log
 from .morphemes import parse_sentence
-from .words import kw_en
+from .words import kw_en, kw_tr
 
 bot = bridge.Bot(command_prefix="/", intents=discord.Intents(messages=True))
 
@@ -19,7 +20,7 @@ bot = bridge.Bot(command_prefix="/", intents=discord.Intents(messages=True))
     description="Provides a naive gloss of a kawaba sentence.",
     guild_ids=config.TEST_GUILDS,
 )
-async def gloss(ctx: commands.Context, *, sentence: str):
+async def gloss(ctx: bridge.BridgeContext, *, sentence: str):
     words = parse_sentence(sentence)
 
     # Concatenating strings with the results we get from gloss_compound
@@ -47,10 +48,41 @@ async def gloss(ctx: commands.Context, *, sentence: str):
     await ctx.reply(f"> {reply}\n{errors}")
 
 
+@bot.bridge_command(description="Searches for a word.", guild_ids=config.TEST_GUILDS)
+async def search(ctx: bridge.BridgeContext, text: str):
+    matches: list[tuple[str, int]] = process.extractBests(
+        text, kw_tr.keys(), score_cutoff=90, limit=50
+    )
+
+    if not matches:
+        await ctx.reply(f"Could not find word `{text}`.")
+        return
+
+    page_template = f"__Found {len(matches)} results__\n"
+
+    reply_pages: list[pages.Page] = []
+
+    # Go through each match 5 at a time and add them as a page
+    while matches:
+        page = page_template
+        for i in range(5):
+            try:
+                match = matches.pop(0)
+                page += f"**{match[0]}** ({match[1]}%)\n> {kw_tr[match[0]]}\n"
+            except IndexError:
+                break
+
+        reply_pages.append(pages.Page(page))
+
+    # Create the paginator and send it
+    paginator = pages.Paginator(reply_pages, show_indicator=True, author_check=True)
+    await paginator.respond(ctx)
+
+
 @bot.bridge_command(
     description="Sends information about this bot.", guild_ids=config.TEST_GUILDS
 )
-async def info(ctx: commands.Context):
+async def info(ctx: bridge.BridgeContext):
     await ctx.reply(
         "Hi, I am **hun kawaba**, a Discord bot for the kawaba language.\n"
         "> `/gloss [sentence]` provides a naive gloss of a kawaba sentence.\n"
